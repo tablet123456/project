41f18cc8e668f5d977928805940750e4ecaaff95
tzvibloom <tzvibloom@TzviBloom>
2018-05-08 17:59:00 +0300

    update

--- src/renderer/Render.java
+++ src/renderer/Render.java
@@ -9,6 +9,49 @@ public class Render {
 	Scene _scene;
 	ImageWriter _imageWriter;
 
+	
+	
+	
+	
+	
+	
+	public Scene get_scene() {
+		return _scene;
+	}
+	
+	public ImageWriter get_imageWriter() {
+		return _imageWriter;
+	}
+	
+	public void set_scene(Scene _scene) {
+		this._scene = _scene;
+	}
+
+	public void set_imageWriter(ImageWriter _imageWriter) {
+		this._imageWriter = _imageWriter;
+	}
+	
+	private Color calcColor(Point3D point) {
+		return _scene.ambientLight.getIntensity();
+		}
+
+
+
+	private Point3D getClosestPoint(List<Point3D> intersectionPoints) {
+		double distance =Double.MAX_VALUE;
+		Point3D p0=_scene.get_camera().get_p0();
+		Point3D minDistancePoint = null;
+		for(Point3D ipoint: intersectionPoints) {
+			if(p0.distance(ipoint)<distance) {
+				minDistancePoint = new Point3D(ipoint);
+				distance=p0.distance(ipoint);
+			}
+		}
+		return minDistancePoint;
+	}
+	
+	
+	
 }
 	
 
---
+++ src/unitTests/ImageWriterTest.java
@@ -0,0 +1,14 @@
+package unitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ImageWriterTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
