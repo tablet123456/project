a7ca6e67b340079adf626311932dd6f4568f6b17
tzvibloom <tzvibloom@TzviBloom>
2018-05-22 17:02:39 +0300

    update

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -78,52 +78,52 @@ public class Vector {
 	}
 	
 	public Vector _add(Vector v) {
-		Coordinate x = new Coordinate(this.getHead().getX().add(v.getHead().getX()));
-		Coordinate y = new Coordinate(this.getHead().getY().add(v.getHead().getY()));
-		Coordinate z = new Coordinate(this.getHead().getZ().add(v.getHead().getZ()));
+		Coordinate x = new Coordinate(getHead().getX().add(v.getHead().getX()));
+		Coordinate y = new Coordinate(getHead().getY().add(v.getHead().getY()));
+		Coordinate z = new Coordinate(getHead().getZ().add(v.getHead().getZ()));
 		return new Vector(x,y,z);
 	}
 	public Vector _subtract(Vector v) {
-	   Coordinate x = new Coordinate(this.getHead().getX().subtract(v.getHead().getX()));
-	   Coordinate y = new Coordinate(this.getHead().getY().subtract(v.getHead().getY()));
-	   Coordinate z = new Coordinate(this.getHead().getZ().subtract(v.getHead().getZ()));
+	   Coordinate x = new Coordinate(getHead().getX().subtract(v.getHead().getX()));
+	   Coordinate y = new Coordinate(getHead().getY().subtract(v.getHead().getY()));
+	   Coordinate z = new Coordinate(getHead().getZ().subtract(v.getHead().getZ()));
 	   return new Vector(x,y,z);
 	}
 
 	public Vector _scale(double _scale) {
-		Coordinate x = this.getHead().getX().scale(_scale);
-		Coordinate y = this.getHead().getY().scale(_scale);
-		Coordinate z = this.getHead().getZ().scale(_scale);
+		Coordinate x = getHead().getX().scale(_scale);
+		Coordinate y = getHead().getY().scale(_scale);
+		Coordinate z = getHead().getZ().scale(_scale);
 		return new Vector(x,y,z);
 		
 	}
 	public double _dotproduct(Vector v) {
-		 Coordinate x = new Coordinate(this.getHead().getX().multiply(v.getHead().getX()));
-		 Coordinate y = new Coordinate(this.getHead().getY().multiply(v.getHead().getY()));
-		 Coordinate z = new Coordinate(this.getHead().getZ().multiply(v.getHead().getZ()));
+		 Coordinate x = new Coordinate(getHead().getX().multiply(v.getHead().getX()));
+		 Coordinate y = new Coordinate(getHead().getY().multiply(v.getHead().getY()));
+		 Coordinate z = new Coordinate(getHead().getZ().multiply(v.getHead().getZ()));
 		 return x.get()+y.get()+z.get();
 	}
 	
 	public Vector _crossproduct(Vector v) {
-		Coordinate x = (this.getHead().getY().multiply(v.getHead().getZ())).subtract(this.getHead().getZ().multiply(v.getHead().getY()));
-		Coordinate y = (this.getHead().getZ().multiply(v.getHead().getX())).subtract(this.getHead().getX().multiply(v.getHead().getZ()));
-		Coordinate z = (this.getHead().getX().multiply(v.getHead().getY())).subtract(this.getHead().getY().multiply(v.getHead().getX()));
+		Coordinate x = (getHead().getY().multiply(v.getHead().getZ())).subtract(getHead().getZ().multiply(v.getHead().getY()));
+		Coordinate y = (getHead().getZ().multiply(v.getHead().getX())).subtract(getHead().getX().multiply(v.getHead().getZ()));
+		Coordinate z = (getHead().getX().multiply(v.getHead().getY())).subtract(getHead().getY().multiply(v.getHead().getX()));
 		return new Vector(x,y,z);
 	}
 	
 	public double _length() {
-		double x= Math.pow(this.getHead().getX().get(), 2);
-		double y= Math.pow(this.getHead().getY().get(), 2);
-		double z= Math.pow(this.getHead().getZ().get(), 2);
+		double x= Math.pow(getHead().getX().get(), 2);
+		double y= Math.pow(getHead().getY().get(), 2);
+		double z= Math.pow(getHead().getZ().get(), 2);
 		double w=x+y+z;
 		return (Math.sqrt(w));
 	}
 	
 	public Vector _normalize() {
 		double length = this.length();
-		double x = (this.getHead().getX().get())/length;
-		double y = (this.getHead().getY().get())/length;
-		double z = (this.getHead().getZ().get())/length;
+		double x = (getHead().getX().get())/length;
+		double y = (getHead().getY().get())/length;
+		double z = (getHead().getZ().get())/length;
 		return new Vector(x, y, z);		
 	}
 
